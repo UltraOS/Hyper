e251100a583ea74d39c0bdabbb321a86dbededea
8infy <99danilt@gmail.com>
2021-10-19 23:46:20 +0300

    Added TTYServices/MemoryServices, refactored code/comments.

--- Loader/Services.h
+++ Loader/Services.h
@@ -1,74 +1,133 @@
 #pragma once
 
-#include <stdint.h>
-#include <stddef.h>
+#include "Common/Types.h"
+#include "Common/StringView.h"
+
+#include "Protocol.h"
 
 class DiskServices {
 public:
     // Reads count sectors at sector offset.
-    // true -> data was read successfully and buffer contents are valid.
-    // false -> failed to read data, buffer contents are undefined.
+    // Returns true if data was read successfully, false otherwise.
     virtual bool read(void* buffer, size_t sector, size_t count) = 0;
 };
 
 struct VideoMode {
-    uint32_t width;
-    uint32_t height;
-    uint32_t id;
+    u32 width;
+    u32 height;
+    u32 id;
 };
 
 struct Resolution {
-    uint32_t width;
-    uint32_t height;
-};
-
-struct Framebuffer {
-    uint32_t width;
-    uint32_t height;
-    uint32_t pitch;
-    uint32_t bpp;
-    uint32_t physical_address;
-};
-
-enum class Platform {
-    BIOS,
-    UEFI
+    u32 width;
+    u32 height;
 };
 
 class VideoServices {
 public:
     // Lists up to max_count modes into buffer.
-    // Retruns the number of modes that could've been listed.
-    // A return value of 0 means no modes could be listed and should be considered fatal.
+    // Returns the number of modes that could've been listed or 0 if
+    // no modes could be listed.
     virtual size_t list_modes(VideoMode* into_buffer, size_t max_count) = 0;
 
-    // Returns the native screen resolution in out_resolution.
-    // true -> successful query, returned resolution is verified to be valid.
-    // false -> either unsuccessful query, or bogus data, out_resolution contents should be ignored.
+    // Attempts to query native screen resolution.
+    // out_resolution -> main display resolution in pixels.
+    // Returns true if query succeeded, false otherwise.
     virtual bool query_resolution(Resolution& out_resolution) = 0;
 
     // Sets one of the modes returned from an earlier call to list_modes().
-    // true -> mode was successfuly set, out_framebuffer is valid.
-    // false -> modesetting failed, out_framebuffer contents should be ignored.
-    virtual bool set_mode(uint32_t id, Framebuffer& out_framebuffer) = 0;
+    // id -> id of the mode to be set.
+    // out_framebuffer -> memory region and various data about the set mode.
+    // Returns true if mode was set successfully, false otherwise.
+    virtual bool set_mode(u32 id, Framebuffer& out_framebuffer) = 0;
+};
+
+enum class TopDown {
+    YES,
+    NO
+};
+
+class MemoryServices {
+public:
+    // Allocates count pages starting at address with type.
+    // address -> page aligned address of the first byte of the range to allocate.
+    // count -> number of 4096-byte pages to allocate.
+    // type -> the type of range to allocate, must be one of the valid protocol values.
+    // Returns the same value as 'address' if allocation succeeded, nullptr otherwise.
+    virtual Address64 allocate_pages_at(Address64 address, size_t count, u32 type) = 0;
+
+    // Allocates count pages with type anywhere in available memory.
+    // count -> number of 4096-byte pages to allocate.
+    // upper_limit -> 1 + maximum allowed address within the allocated range.
+    // type -> the type of range to allocate, must be one of the valid protocol values.
+    // TopDown -> indicates whether range should be allocated at the highest available region of memory
+    //            but under upper_limit. If set to NO the lowest available range is picked.
+    // Returns the address of the first byte of the allocated range if allocation succeeded, nullptr otherwise.
+    virtual Address64 allocate_pages(size_t count, Address64 upper_limit, u32 type, TopDown) = 0;
+
+    // Frees count pages starting at address.
+    virtual void free_pages(Address64 address, size_t count) = 0;
+
+    // Copies protocol-formatted memory map entries into buffer.
+    // into_buffer -> pointer to the first byte of the buffer that receives memory map entries
+    //                (allowed to be nullptr if capacity is passed as 0).
+    // capacity_in_bytes -> capacity of the buffer.
+    // key -> set by the service provider and is a unique id of the current state of the map
+    //        that changes with every allocate/free call. Only set if capacity_in_bytes
+    //        was enough to receive the entire map.
+    // Returns the number of bytes that would've been copied if buffer had enough capacity.
+    virtual size_t copy_map(void* into_buffer, size_t capacity_in_bytes, size_t& out_key) = 0;
+
+    // Disables the service and makes caller the owner of the entire map.
+    // key -> expected id of the current state of the memory map.
+    // Returns true if key handover was successful and key matched the internal state,
+    // otherwise key didn't match the internal state and memory map must be refetched.
+    virtual bool handover(size_t key) = 0;
+};
+
+enum class Color {
+    WHITE,
+    GRAY,
+    YELLOW,
+    RED,
+    BLUE,
+    GREEN,
+};
+
+class TTYServices {
+public:
+    // Writes string to the output device with the given color.
+    // test -> ascii string to output to the device.
+    // color -> color of the output message.
+    // Returns true if string was successfully written, false otherwise.
+    virtual bool write(StringView text, Color color) = 0;
+
+    // Returns the output device resolution in characters.
+    virtual Resolution resolution() const = 0;
+
+    // Returns true if the service is currently available.
+    // This might change after setting a video mode.
+    virtual bool is_available() const = 0;
 };
 
 class Services {
 public:
-    Services(Platform platform, DiskServices& disk_services, VideoServices& video_services)
-        : m_platform(platform)
-        , m_disk_services(disk_services)
+    Services(DiskServices& disk_services, VideoServices& video_services, MemoryServices& memory_services, TTYServices& tty_services)
+        : m_disk_services(disk_services)
         , m_video_services(video_services)
+        , m_memory_services(memory_services)
+        , m_tty_services(tty_services)
     {
     }
 
-    Platform platform() const { return m_platform; }
-
     DiskServices& disk_services() { return m_disk_services; }
     VideoServices& video_services() { return m_video_services; }
+    MemoryServices& memory_services() { return m_memory_services; }
+    TTYServices& tty_services() { return m_tty_services; }
 
 private:
-    Platform m_platform;
     DiskServices& m_disk_services;
     VideoServices& m_video_services;
+    MemoryServices& m_memory_services;
+    TTYServices& m_tty_services;
 };
