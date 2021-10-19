690e24f9692611696da5a02cc96e906edec944d2
8infy <99danilt@gmail.com>
2021-10-19 23:24:47 +0300

    Added Common, moved Types into it. Imported BasicAddress, ssize_t.

--- Loader/Common/Types.h
+++ Loader/Common/Types.h
@@ -17,6 +17,12 @@ using u64 = uint64_t;
 
 using ptr_t = size_t;
 
+#ifdef __i386__
+using ssize_t = i32;
+#else
+using ssize_t = i64;
+#endif
+
 #define SIZEOF_I8 1
 #define SIZEOF_I16 2
 #define SIZEOF_I32 4
@@ -50,3 +56,55 @@ static_assert(sizeof(u64) == SIZEOF_U64, "Incorrect size of 64 bit unsigned inte
 #define KB (1024ul)
 #define MB (1024ul * KB)
 #define GB (1024ul * MB)
+
+template <typename SizeT>
+class BasicAddress {
+public:
+    BasicAddress() = default;
+
+    using underlying_pointer_type = SizeT;
+
+    template <typename T>
+    constexpr BasicAddress(T* ptr)
+        : m_ptr(reinterpret_cast<ptr_t>(ptr))
+    {
+    }
+
+    constexpr BasicAddress(decltype(nullptr))
+        : m_ptr(0)
+    {
+    }
+
+    constexpr BasicAddress(SizeT address)
+        : m_ptr(address)
+    {
+    }
+
+    constexpr underlying_pointer_type raw() const { return m_ptr; }
+
+    template <typename T>
+    constexpr T* as_pointer()
+    {
+        return reinterpret_cast<T*>(m_ptr);
+    }
+
+    template <typename T>
+    constexpr T* as_pointer() const
+    {
+        return const_cast<T*>(reinterpret_cast<const T*>(m_ptr));
+    }
+
+    constexpr operator underlying_pointer_type() const { return m_ptr; }
+
+    void operator+=(size_t offset) { m_ptr += offset; }
+    void operator-=(size_t offset) { m_ptr -= offset; }
+    void operator|=(underlying_pointer_type value) { m_ptr |= value; }
+    void operator&=(underlying_pointer_type value) { m_ptr &= value; }
+
+private:
+    SizeT m_ptr { 0 };
+};
+
+using Address = BasicAddress<ptr_t>;
+using Address32 = BasicAddress<u32>;
+using Address64 = BasicAddress<u64>;
