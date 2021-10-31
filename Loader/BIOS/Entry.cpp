12bca45675fef5896c1edda7a5c30d634cbca478
8infy <99danilt@gmail.com>
2021-10-31 15:47:03 +0300

    Implemented BIOS entry.

--- Loader/BIOS/Entry.cpp
+++ Loader/BIOS/Entry.cpp
@@ -1,5 +1,31 @@
+#include "Common/Logger.h"
+#include "Common/Runtime.h"
+#include "Common/Utilities.h"
+
+#include "BIOSMemoryServices.h"
+#include "BIOSVideoServices.h"
+#include "BIOSDiskServices.h"
+
+extern "C" bool a20_enabled;
+extern "C" u8 section_bss_begin;
+extern "C" u8 section_bss_end;
+
 extern "C" void bios_entry()
 {
-    // TODO
-    for (;;);
+    zero_memory(&section_bss_begin, &section_bss_end - &section_bss_begin);
+
+    auto video_services = BIOSVideoServices::create();
+    logger::set_backend(&video_services);
+
+    if (!a20_enabled)
+        panic("Failed to enable A20! Please report this issue.");
+
+    video_services.fetch_all_video_modes();
+    video_services.fetch_native_resolution();
+
+    auto memory_services = BIOSMemoryServices::create();
+    auto disk_services = BIOSDiskServices::create();
+
+    Services services(disk_services, video_services, memory_services);
+    loader_entry(services);
 }
