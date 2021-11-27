028d812f05e20519adf4cf4a6f7aa30a8a7e6785
8infy <99danilt@gmail.com>
2021-11-27 23:06:43 +0300

    A bunch of new ELF structures.

--- Loader/ELF/Structures.h
+++ Loader/ELF/Structures.h
@@ -83,3 +83,48 @@ struct Elf64_Ehdr {
     Elf64_Half e_shnum;
     Elf64_Half e_shstrndx;
 };
+
+static constexpr u32 PT_NULL = 0;
+static constexpr u32 PT_LOAD = 1;
+static constexpr u32 PT_DYNAMIC = 2;
+static constexpr u32 PT_INTERP = 3;
+static constexpr u32 PT_NOTE = 4;
+static constexpr u32 PT_SHLIB = 5;
+static constexpr u32 PT_PHDR = 6;
+static constexpr u32 PT_TLS = 7;
+static constexpr u32 PT_LOOS = 0x60000000;
+static constexpr u32 PT_SUNW_UNWIND = 0x6464e550;
+static constexpr u32 PT_SUNW_EH_FRAME = 0x6474e550;
+static constexpr u32 PT_LOSUNW = 0x6ffffffa;
+static constexpr u32 PT_SUNWBSS = 0x6ffffffa;
+static constexpr u32 PT_SUNWSTACK = 0x6ffffffb;
+static constexpr u32 PT_SUNWDTRACE = 0x6ffffffc;
+static constexpr u32 PT_SUNWCAP = 0x6ffffffd;
+static constexpr u32 PT_HISUNW = 0x6fffffff;
+static constexpr u32 PT_HIOS = 0x6fffffff;
+static constexpr u32 PT_LOPROC = 0x70000000;
+static constexpr u32 PT_HIPROC = 0x7fffffff;
+
+static constexpr u16 PN_XNUM = 0xFFFF;
+
+struct Elf32_Phdr {
+    Elf32_Word p_type;
+    Elf32_Off p_offset;
+    Elf32_Addr p_vaddr;
+    Elf32_Addr p_paddr;
+    Elf32_Word p_filesz;
+    Elf32_Word p_memsz;
+    Elf32_Word p_flags;
+    Elf32_Word p_align;
+};
+
+struct Elf64_Phdr {
+    Elf64_Word p_type;
+    Elf64_Word p_flags;
+    Elf64_Off p_offset;
+    Elf64_Addr p_vaddr;
+    Elf64_Addr p_paddr;
+    Elf64_Xword p_filesz;
+    Elf64_Xword p_memsz;
+    Elf64_Xword p_align;
+};
