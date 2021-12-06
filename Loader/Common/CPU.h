413b3ce371519fd08a9b6f88b59f39646deabae5
8infy <99danilt@gmail.com>
2021-12-06 23:46:13 +0300

    Added cpu::supports_long_mode()

--- Loader/Common/CPU.h
+++ Loader/Common/CPU.h
@@ -2,6 +2,8 @@
 
 #include "Types.h"
 
+namespace cpu {
+
 struct ID {
     u32 a { 0x00000000 };
     u32 b { 0x00000000 };
@@ -9,7 +11,7 @@ struct ID {
     u32 d { 0x00000000 };
 };
 
-inline ID cpu_id(u32 function)
+inline ID id(u32 function)
 {
     ID out {};
 
@@ -18,5 +20,21 @@ inline ID cpu_id(u32 function)
         : "a"(function), "c"(0));
 
     return out;
+}
+
+inline bool supports_long_mode()
+{
+    static constexpr u32 highest_implemented_function_number = 0x80000000;
+    static constexpr u32 extended_processor_info_function_number = 0x80000001;
+
+    auto highest_function = id(highest_implemented_function_number).a;
+
+    if (highest_function < extended_processor_info_function_number)
+        return false;
+
+    static constexpr u32 long_mode_bit = SET_BIT(29);
+    return id(extended_processor_info_function_number).d & long_mode_bit;
+}
+
 }
 
