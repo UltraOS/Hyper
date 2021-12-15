69f576e5189302691e57266d7b92d4a72ee7ec76
8infy <99danilt@gmail.com>
2021-12-15 22:17:49 +0300

    Added a few new attributes & changed style.

--- Protocol.h
+++ Protocol.h
@@ -10,7 +10,7 @@
 #define ATTRIBUTE_FRAMEBUFFER_INFO 5
 #define ATTRIBUTE_END              6
 
-struct AttributeHeader {
+struct attribute_header {
     uint32_t type;
     uint32_t size_in_bytes;
 };
@@ -19,12 +19,46 @@ struct AttributeHeader {
 #define PLATFORM_BIOS    1
 #define PLATFORM_UEFI    2
 
-struct PlatformInfoAttribute {
-    struct AttributeHeader header;
+struct platform_info_attribute {
+    struct attribute_header header;
     uint32_t platform_type;
+
     uint16_t loader_major;
     uint16_t loader_minor;
     char loader_name[32];
+
+    u64 acpi_rsdp_address;
+};
+
+#define PARTITION_TYPE_RAW 1
+#define PARTITION_TYPE_MBR 2
+#define PARTITION_TYPE_GPT 3
+
+struct guid {
+    uint32_t data1;
+    uint16_t data2;
+    uint16_t data3;
+    uint8_t  data4[8];
+};
+
+struct kernel_info_attribute {
+    struct attribute_header header;
+
+    u64 physical_base;
+    u64 virtual_base;
+    u64 range_length;
+
+    u64 partition_type;
+
+    // only valid if partition_type == PARTITION_TYPE_GPT
+    struct guid disk_guid;
+    struct guid partition_guid;
+
+    // always valid
+    u32 disk_index;
+    u32 partition_index;
+
+    char path_on_disk[256];
 };
 
 #define MEMORY_TYPE_INVALID            0
@@ -37,27 +71,28 @@ struct PlatformInfoAttribute {
 #define MEMORY_TYPE_KERNEL_STACK       7
 #define MEMORY_TYPE_KERNEL_BINARY      8
 
-struct MemoryMapEntry {
+struct memory_map_entry {
     uint64_t physical_address;
     uint64_t size_in_bytes;
     uint64_t type;
 };
 #define MEMORY_MAP_ENTRY_COUNT(header) ((((header).size_in_bytes) - sizeof(AttributeHeader)) / sizeof(MemoryMapEntry))
 
-struct MemoryMapAttribute {
-    struct AttributeHeader header;
-    struct MemoryMapEntry entries[];
+struct memory_map_attribute {
+    struct attribute_header header;
+    struct memory_map_entry entries[];
 };
 
-struct ModuleInfoAttribute {
-    struct AttributeHeader header;
+struct module_info_attribute {
+    struct attribute_header header;
     char name[64];
     uint64_t physical_address;
     uint64_t length;
 };
 
-struct CommandLineAttribute {
-    struct AttributeHeader header;
+struct command_line_attribute {
+    struct attribute_header header;
+    uint32_t text_length;
     char text[];
 };
 #define COMMAND_LINE_LENGTH(header) (((header).size_in_bytes) - sizeof(AttributeHeader))
@@ -66,22 +101,23 @@ struct CommandLineAttribute {
 #define FORMAT_RBG     1
 #define FORMAT_RGBA    2
 
-struct Framebuffer {
+struct framebuffer {
     uint32_t width;
     uint32_t height;
     uint32_t pitch;
-    uint32_t bpp;
-    uint32_t format;
+    uint16_t bpp;
+    uint16_t format;
     uint64_t physical_address;
 };
 
-struct FramebufferAttribute {
-    struct AttributeHeader header;
-    struct Framebuffer framebuffer;
+struct framebuffer_attribute {
+    struct attribute_header header;
+    struct framebuffer framebuffer;
 };
 
-struct BootContext {
-    struct AttributeHeader* attributes;
+struct boot_context {
+    u64 attribute_count;
+    struct attribute_header attributes[];
 };
 #define NEXT_ATTRIBUTE(current) ((AttributeHeader*)(((uint8_t*)(current)) + (current)->size_in_bytes))
 
