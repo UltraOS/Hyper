e4ca84b59c7a95b3da5238db0724f17dd503ee3b
8infy <99danilt@gmail.com>
2021-12-17 21:28:00 +0300

    Move cpuid functions out of line & simplify.

---
+++ Loader/Common/CPU.cpp
@@ -0,0 +1,24 @@
+#include "CPU.h"
+
+void cpuid(u32 function, struct CPUID *id)
+{
+    asm volatile("cpuid"
+        : "=a"(id->a), "=b"(id->b), "=c"(id->c), "=d"(id->d)
+        : "a"(function), "c"(0));
+}
+
+#define HIGHEST_IMPLEMENTED_FUNCTION_NUMBER 0x80000000
+#define EXTENDED_PROCESSOR_INFO_FUNCTION_NUMBER 0x80000001
+#define LONG_MODE_BIT (1 << 29)
+
+bool cpu_supports_long_mode()
+{
+    struct CPUID id = { 0 };
+
+    cpuid(HIGHEST_IMPLEMENTED_FUNCTION_NUMBER, &id);
+    if (id.a < EXTENDED_PROCESSOR_INFO_FUNCTION_NUMBER)
+        return false;
+
+    cpuid(EXTENDED_PROCESSOR_INFO_FUNCTION_NUMBER, &id);
+    return id.d & LONG_MODE_BIT;
+}
--- Loader/Common/CPU.h
+++ Loader/Common/CPU.h
@@ -2,39 +2,12 @@
 
 #include "Types.h"
 
-namespace cpu {
-
-struct ID {
-    u32 a { 0x00000000 };
-    u32 b { 0x00000000 };
-    u32 c { 0x00000000 };
-    u32 d { 0x00000000 };
+struct CPUID {
+    u32 a;
+    u32 b;
+    u32 c;
+    u32 d;
 };
 
-inline ID id(u32 function)
-{
-    ID out {};
-
-    asm volatile("cpuid"
-        : "=a"(out.a), "=b"(out.b), "=c"(out.c), "=d"(out.d)
-        : "a"(function), "c"(0));
-
-    return out;
-}
-
-inline bool supports_long_mode()
-{
-    static constexpr u32 highest_implemented_function_number = 0x80000000;
-    static constexpr u32 extended_processor_info_function_number = 0x80000001;
-
-    auto highest_function = id(highest_implemented_function_number).a;
-
-    if (highest_function < extended_processor_info_function_number)
-        return false;
-
-    static constexpr u32 long_mode_bit = SET_BIT(29);
-    return id(extended_processor_info_function_number).d & long_mode_bit;
-}
-
-}
-
+void cpuid(u32 function, struct CPUID *id);
+bool cpu_supports_long_mode();
