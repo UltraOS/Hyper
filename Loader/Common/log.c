36eb3471aee4b37e13f25cc85843513d58db6e99
8infy <99danilt@gmail.com>
2021-12-31 00:14:02 +0300

    added log to replace the current logger

---
+++ Loader/Common/log.c
@@ -0,0 +1,77 @@
+#include "log.h"
+#include "format.h"
+#include "string.h"
+
+static struct video_services *log_backend = NULL;
+static enum log_level current_level = LOG_LEVEL_INFO;
+
+enum log_level logger_set_level(enum log_level level)
+{
+    enum log_level prev = current_level;
+    current_level = level;
+    return prev;
+}
+
+struct video_services *logger_set_backend(struct video_services *s)
+{
+    struct video_services *prev = log_backend;
+    log_backend = s;
+    return prev;
+}
+
+static int extract_message_level(const char **msg_ptr)
+{
+    const char *msg = *msg_ptr;
+
+    if (msg[0] != LOG_LEVEL_PREFIX[0] || !msg[1])
+        return LOG_LEVEL_INFO;
+
+    switch (msg[1]) {
+    case '0' ... '3':
+        *msg_ptr += 2;
+        return msg[1] - '0';
+    }
+
+    return LOG_LEVEL_INFO;
+}
+
+static enum color get_color_for_level(int level)
+{
+    switch (level) {
+        default:
+        case LOG_LEVEL_INFO:
+            return COLOR_GRAY;
+        case LOG_LEVEL_WARN:
+            return COLOR_YELLOW;
+        case LOG_LEVEL_ERR:
+            return COLOR_RED;
+    }
+}
+
+void vprint(const char *msg, va_list vlist)
+{
+    static char log_buf[256];
+    int chars;
+    int level;
+    enum color col;
+
+    if (unlikely(!msg || !log_backend))
+        return;
+
+    level = extract_message_level(&msg);
+    if (level < current_level)
+        return;
+
+    col = get_color_for_level(level);
+
+    chars = vscnprintf(log_buf, sizeof(log_buf), msg, vlist);
+    log_backend->tty_write(log_buf, chars, col);
+}
+
+void print(const char *msg, ...)
+{
+    va_list vlist;
+    va_start(vlist, msg);
+    vprint(msg, vlist);
+    va_end(vlist);
+}
---
+++ Loader/Common/log.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "services.h"
+
+// Inspired by linux kern_levels
+#define LOG_LEVEL_PREFIX "\x1"
+#define LOG_INFO LOG_LEVEL_PREFIX "1"
+#define LOG_WARN LOG_LEVEL_PREFIX "2"
+#define LOG_ERR  LOG_LEVEL_PREFIX "3"
+
+enum log_level {
+    LOG_LEVEL_INFO = 1,
+    LOG_LEVEL_WARN = 2,
+    LOG_LEVEL_ERR = 3,
+};
+
+enum log_level logger_set_level(enum log_level level);
+struct video_services *logger_set_backend(struct video_services*);
+
+void vprintlvl(enum log_level, const char *msg, va_list vlist);
+void vprint(const char *msg, va_list vlist);
+
+PRINTF_DECL(2, 3)
+void printlvl(enum log_level, const char *msg, ...);
+
+PRINTF_DECL(1, 2)
+void print(const char *msg, ...);
+
+#ifndef MSG_FMT
+#define MSG_FMT(msg) msg
+#endif
+
+#define print_info(msg, ...) print(LOG_INFO MSG_FMT(msg), ##__VA_ARGS__)
+#define print_warn(msg, ...) print(LOG_WARN MSG_FMT(msg), ##__VA_ARGS__)
+#define print_err(msg, ...)  print(LOG_ERR MSG_FMT(msg), ##__VA_ARGS__)
+
+
+
