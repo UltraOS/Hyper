428c27d010b9888fcb45c9db6af22cad3879f775
8infy <99danilt@gmail.com>
2022-01-03 00:13:14 +0300

    Remove a bunch of c++-specific common utilities

--- Loader/Common/Panic.cpp
+++
@@ -1,9 +0,0 @@
-#include "Panic.h"
-#include "Runtime.h"
-
-u8 in_panic_depth = 0;
-
-void do_panic()
-{
-    hang();
-}
--- Loader/Common/Runtime.cpp
+++
@@ -1,8 +0,0 @@
-#include "Runtime.h"
-#include "Panic.h"
-
-[[noreturn]] void on_assertion_failed(const char* message, const char* file, const char* function, u32 line)
-{
-    panic("Assertion failed!\nexpression: {}\nat {}:{} -> {}",
-          message, file, line, function);
-}
