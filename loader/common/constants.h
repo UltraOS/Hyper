31032acde6dec9d1485ce63af7a3f2369993ea5f
8infy <99danilt@gmail.com>
2022-01-04 12:41:27 +0300

    Added {HUGE_}PAGE_ROUND_DOWN

--- loader/common/constants.h
+++ loader/common/constants.h
@@ -7,7 +7,9 @@
 #define PAGE_SIZE 4096
 #define PAGE_ROUND_UP(size) \
     (size & (PAGE_SIZE - 1)) ? ((size + PAGE_SIZE) & ~PAGE_SIZE) : size
+#define PAGE_ROUND_DOWN(size) (size & (PAGE_SIZE - 1))
 
 #define HUGE_PAGE_SIZE (2 * MB)
 #define HUGE_PAGE_ROUND_UP(size) \
     (size & (HUGE_PAGE_SIZE - 1)) ? ((size + HUGE_PAGE_SIZE) & ~HUGE_PAGE_SIZE) : size
+#define HUGE_PAGE_ROUND_DOWN(size) (size & (HUGE_PAGE_SIZE - 1))
