2a095084c3e8f6e53c975f4ea8ef75a89b81cc25
8infy <99danilt@gmail.com>
2022-01-15 17:26:50 +0300

    Added OOPS_ON & panic fixes.

--- loader/common/panic.c
+++ loader/common/panic.c
@@ -5,7 +5,7 @@
 void panic(const char *reason, ...)
 {
     va_list vlist;
-    va_start(reason, vlist);
+    va_start(vlist, reason);
     vprintlvl(LOG_LEVEL_ERR, reason, vlist);
     va_end(vlist);
 
@@ -17,7 +17,7 @@ void oops(const char *reason, ...)
     print_err("Oops!\n");
 
     va_list vlist;
-    va_start(reason, vlist);
+    va_start(vlist, reason);
     vprintlvl(LOG_LEVEL_ERR, reason, vlist);
     va_end(vlist);
 
--- loader/common/panic.h
+++ loader/common/panic.h
@@ -9,3 +9,9 @@ void panic(const char *reason, ...);
 NORETURN
 PRINTF_DECL(1, 2)
 void oops(const char *reason, ...);
+
+#define OOPS_ON(expr)                            \
+    do {                                         \
+        if (unlikely(expr))                      \
+            oops(#expr " evaluated to true");    \
+    } while (0)
