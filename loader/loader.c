eaa47b880ca3ceb42f59e08613eca07e7714eb7a
8infy <99danilt@gmail.com>
2022-01-15 17:34:39 +0300

    C-rewrite: loader.c

--- loader/loader.c
+++ loader/loader.c
@@ -1,15 +1,18 @@
 #include "services.h"
 #include "common/log.h"
 #include "common/panic.h"
+#include "common/constants.h"
+#include "common/helpers.h"
+#include "common/string_view.h"
 #include "allocator.h"
 #include "filesystem/filesystem.h"
 #include "filesystem/filesystem_table.h"
-#include "protocols/ultra/ultra.h"
+#include "protocols/ultra.h"
 #include "config.h"
 
-void detect_all_filesystems(struct disk_services*, const struct disk* d, u32 disk_index);
-struct file* find_config_file(struct fs_entry *fe);
-void pick_loadable_entry(struct config *cfg, struct loadable_entry* le);
+void detect_all_filesystems(struct disk_services*, const struct disk *d, u32 disk_index);
+struct file *find_config_file(struct fs_entry **fe);
+void pick_loadable_entry(struct config *cfg, struct loadable_entry *le);
 
 enum load_protocol {
     LOAD_PROTOCOL_ULTRA,
@@ -21,10 +24,10 @@ void loader_entry(struct services *svc)
 {
     size_t disk_count, disk_index;
     struct disk *disks;
-    struct fs_entry fe;
+    struct fs_entry *fe;
     struct file *cfg_file;
     char *cfg_data;
-    struct string_view cfg_view = { cfg_data, 0 };
+    struct string_view cfg_view;
     struct config cfg = { 0 };
     struct loadable_entry le;
     enum load_protocol prot;
@@ -33,7 +36,7 @@ void loader_entry(struct services *svc)
     allocator_set_backend(svc->ms);
     filesystem_set_backend(svc->ds);
 
-    svc->ds->list_disks(&disk_count);
+    disks = svc->ds->list_disks(&disk_count);
     for (disk_index = 0; disk_index < disk_count; ++disk_index)
         detect_all_filesystems(svc->ds, &disks[disk_index], disk_index);
 
@@ -41,7 +44,7 @@ void loader_entry(struct services *svc)
     if (!cfg_file)
         oops("Couldn't find ultra.cfg anywhere on disk!");
 
-    set_origin_fs(&fe);
+    set_origin_fs(fe);
 
     cfg_data = allocate_bytes(cfg_file->size);
     if (!cfg_data)
@@ -49,188 +52,185 @@ void loader_entry(struct services *svc)
 
     if (!cfg_file->read(cfg_file, cfg_data, 0, cfg_file->size))
         oops("failed to read config file");
-    cfg_view.size = cfg_file->size;
+    cfg_view = (struct string_view) {
+        .text = cfg_data,
+        .size = cfg_file->size
+    };
 
-    if (!config_parse(cfg_view, &cfg)) {
-        config_pretty_print_error(&cfg.last_error, cfg_view);
+    if (!cfg_parse(cfg_view, &cfg)) {
+        cfg_pretty_print_error(&cfg.last_error, cfg_view);
         for (;;);
     }
 
     pick_loadable_entry(&cfg, &le);
     prot = deduce_protocol(&cfg, &le);
 
-    switch (prot) {
-    case LOAD_PROTOCOL_ULTRA:
-        ultra_protocol_load(cfg, loadable_entry, services);
-    default:
-        panic("invalid protocol");
-    }
+    BUG_ON(prot != LOAD_PROTOCOL_ULTRA);
+    ultra_protocol_load(&cfg, &le, svc);
 }
 
-void initialize_from_mbr(DiskServices& srvc, const Disk& disk, u32 disk_id, void* mbr_buffer, size_t base_index = 0, size_t sector_offset = 0)
+struct PACKED mbr_partition_entry {
+    u8 status;
+    u8 chs_begin[3];
+    u8 type;
+    u8 chs_end[3];
+    u32 first_block;
+    u32 block_count;
+};
+BUILD_BUG_ON(sizeof(struct mbr_partition_entry) != 16);
+
+enum {
+    MBR_EMPTY_PARTITION = 0x00,
+    MBR_EBR_PARTITION   = 0x05
+};
+
+#define OFFSET_TO_MBR_PARTITION_LIST 0x01BE
+
+void initialize_from_mbr(struct disk_services *srvc, const struct disk *d, u32 disk_id,
+                         void *mbr_buffer, size_t base_index, size_t sector_offset)
 {
-    static constexpr u8 empty_partition_type = 0x00;
-    static constexpr u8 ebr_partition_type = 0x05;
-
-    struct PACKED MBRPartitionEntry {
-        u8 status;
-        u8 chs_begin[3];
-        u8 type;
-        u8 chs_end[3];
-        u32 first_block;
-        u32 block_count;
-    };
-    static_assert(sizeof(MBRPartitionEntry) == 16);
-
-    static constexpr size_t offset_to_partitions = 0x01BE;
-    Address mbr_address = mbr_buffer;
-    mbr_address += offset_to_partitions;
-    auto* partition = mbr_address.as_pointer<MBRPartitionEntry>();
-
+    struct mbr_partition_entry *partition = mbr_buffer + OFFSET_TO_MBR_PARTITION_LIST;
     bool is_ebr = base_index != 0;
     size_t max_partitions = is_ebr ? 2 : 4;
 
     for (size_t i = 0; i < max_partitions; ++i, ++partition) {
-        if (partition->type == empty_partition_type)
+        size_t real_partition_offset = sector_offset + partition->first_block;
+        struct range lba_range = { real_partition_offset, partition->block_count };
+        void *partition_page;
+        struct filesystem *fs = NULL;
+
+        if (partition->type == MBR_EMPTY_PARTITION)
             continue;
 
-        auto real_offset = sector_offset + partition->first_block;
-
-        if (i == 1)
-            warnln("index type 1 ({}), base {}", partition->type, base_index);
-
-        if (partition->type == ebr_partition_type) {
+        if (partition->type == MBR_EBR_PARTITION) {
             if (is_ebr && i == 0) {
-                warnln("EBR with chain at index 0");
+                print_warn("EBR with chain at index 0");
                 break;
             }
 
-            auto ebr_page = allocator::ScopedPageAllocation(1);
-            if (ebr_page.failed())
+            partition_page = allocate_pages(1);
+            if (!partition_page)
                 break;
 
-            if (srvc.read_blocks(disk.handle, ebr_page.address(), real_offset, page_size / disk.bytes_per_sector))
-                initialize_from_mbr(srvc, disk, disk_id, ebr_page.address(), base_index + (is_ebr ? 1 : 4), real_offset);
-
+            if (srvc->read_blocks(d->handle, partition_page, real_partition_offset, PAGE_SIZE / d->bytes_per_sector)) {
+                initialize_from_mbr(srvc, d, disk_id, partition_page, base_index + (is_ebr ? 1 : 4),
+                                    real_partition_offset);
+            }
+            free_pages(partition_page, 1);
             continue;
         }
 
         if (i == 1 && is_ebr) {
-            warnln("EBR with a non-EBR entry at index 1 ({})", partition->type);
+            print_warn("EBR with a non-EBR entry at index 1 (0x%X)", partition->type);
             break;
         }
 
-        allocator::ScopedPageAllocation first_partition_page(1);
-        if (first_partition_page.failed())
+        partition_page = allocate_pages(1);
+        if (!partition_page)
             break;
 
-        FileSystem* fs = nullptr;
-        LBARange range { real_offset, partition->block_count };
-
-        if (srvc.read_blocks(disk.handle, first_partition_page.address(), range.begin(), page_size / disk.bytes_per_sector))
-            fs = FileSystem::try_detect(disk, range, first_partition_page.address());
+        if (srvc->read_blocks(d->handle, partition_page, lba_range.begin, PAGE_SIZE / d->bytes_per_sector))
+            fs = fs_try_detect(d, lba_range, partition_page);
 
         if (fs)
-            fs_table::add_mbr_entry(disk.handle, disk_id, base_index + i, fs);
+            add_mbr_fs_entry(d->handle, disk_id, base_index + i, fs);
+
+        free_pages(partition_page, 1);
     }
 }
 
-void detect_all_filesystems(DiskServices& srvc, const Disk& disk, u32 disk_id)
+#define GPT_SIGNATURE "EFI PART"
+#define OFFSET_TO_GPT_SIGNATURE 512
+
+#define MBR_SIGNATURE 0xAA55
+#define OFFSET_TO_MBR_SIGNATURE 510
+
+void detect_all_filesystems(struct disk_services *srvc, const struct disk *d, u32 disk_id)
 {
+    void *table_page;
+
     // Currently unsupported
-    if (disk.bytes_per_sector != 512)
+    if (d->bytes_per_sector != 512)
         return;
 
-    allocator::ScopedPageAllocation first_page(1);
-    if (first_page.failed())
+    table_page = allocate_pages(1);
+    if (!table_page)
         return;
-    if (!srvc.read_blocks(disk.handle, first_page.address(), 0, page_size / 512))
+    if (!srvc->read_blocks(d->handle, table_page, 0, PAGE_SIZE / 512))
         return;
 
-    static constexpr StringView gpt_signature = "EFI PART";
-    static constexpr size_t offset_to_gpt_signature = 512;
-
-    auto* gpt_signature_data = first_page.as_pointer<const char>() + offset_to_gpt_signature;
-
-    if (gpt_signature == gpt_signature_data) {
-        warnln("GPT-partitioned drive {x} skipped", disk.handle);
+    if (!memcmp(table_page + OFFSET_TO_GPT_SIGNATURE, GPT_SIGNATURE, sizeof(GPT_SIGNATURE) - 1)) {
+        print_warn("GPT-partitioned drive %p skipped", d->handle);
         return;
     }
 
-    static constexpr u16 mbr_signature = 0xAA55;
-    static constexpr size_t offset_to_mbr_signature = 510;
-
-    auto* mbr_signature_data = first_page.as_pointer<u16>() + (offset_to_mbr_signature / sizeof(u16));
-
-    if (*mbr_signature_data != mbr_signature) {
-        warnln("unpartitioned drive {x} skipped", disk.handle);
+    if (*(u16*)(table_page + OFFSET_TO_MBR_SIGNATURE) != MBR_SIGNATURE) {
+        print_warn("unpartitioned drive %p skipped", d->handle);
         return;
     }
 
-    initialize_from_mbr(srvc, disk, disk_id, first_page.address());
+    initialize_from_mbr(srvc, d, disk_id, table_page, 0, 0);
+    free_pages(table_page, 1);
 }
 
-File* find_config_file(FileSystemEntry& out_entry)
-{
-    StringView search_paths[] = {
-        "/ultra.cfg",
-        "/boot/ultra.cfg",
-        "/boot/ultra/ultra.cfg",
-        "/boot/Ultra/ultra.cfg",
-        "/Boot/ultra.cfg",
-        "/Boot/ultra/ultra.cfg",
-        "/Boot/Ultra/ultra.cfg",
-    };
+static struct string_view search_paths[] = {
+    SV_CONSTEXPR("/ultra.cfg"),
+    SV_CONSTEXPR("/boot/ultra.cfg"),
+    SV_CONSTEXPR("/boot/ultra/ultra.cfg"),
+    SV_CONSTEXPR("/boot/Ultra/ultra.cfg"),
+    SV_CONSTEXPR("/Boot/ultra.cfg"),
+    SV_CONSTEXPR("/Boot/ultra/ultra.cfg"),
+    SV_CONSTEXPR("/Boot/Ultra/ultra.cfg"),
+};
 
-    for (auto& entry : fs_table::all()) {
-        for (auto path : search_paths) {
-            auto* file = entry.filesystem->open(path);
-            if (!file)
+struct file *find_config_file(struct fs_entry **out_entry)
+{
+    struct fs_entry *entries;
+    size_t i, j, entry_count;
+    entries = list_fs_entries(&entry_count);
+
+    for (i = 0; i < entry_count; ++i) {
+        for (j = 0; j < ARRAY_SIZE(search_paths); ++j) {
+            struct filesystem *fs = entries[i].fs;
+            struct file *f = fs->open(fs, search_paths[i]);
+
+            if (!f)
                 continue;
 
-            out_entry = entry;
-            return file;
+            *out_entry = &entries[i];
+            return f;
         }
     }
 
-    return nullptr;
+    return NULL;
 }
 
-LoadableEntry pick_loadable_entry(Config& cfg)
+#define DEFAULT_ENTRY_KEY SV("default-entry")
+
+void pick_loadable_entry(struct config *cfg, struct loadable_entry *le)
 {
-    static constexpr StringView key_for_default_entry = "default-entry";
+    struct string_view loadable_entry_name;
 
-    auto default_entry = cfg.get(key_for_default_entry);
-    if (!default_entry) {
-        auto entries = cfg.loadable_entries();
-        if (entries.begin() == entries.end())
-            unrecoverable_error("configuration file must contain at least one loadable entry");
-
-        return *entries.begin();
+    if (!cfg_get_global_string(cfg, DEFAULT_ENTRY_KEY, &loadable_entry_name)) {
+        if (!cfg_first_loadable_entry(cfg, le))
+            oops("configuration file must contain at least one loadable entry");
+        return;
     }
 
-    auto entry_string = extract_string({ key_for_default_entry, default_entry.value() });
-
-    auto entry = cfg.get_loadable_entry(entry_string);
-    if (!entry)
-        unrecoverable_error("Couldn't find loadable entry {}", entry_string);
-
-    return entry.value();
+    if (!cfg_get_loadable_entry(cfg, loadable_entry_name, le))
+        oops("no loadable entry called %pSV", &loadable_entry_name);
 }
 
 #define PROTOCOL_KEY SV("protocol")
 
 enum load_protocol deduce_protocol(struct config *cfg, struct loadable_entry *entry)
 {
-    struct value protocol_value;
-    if (!loadable_entry_get_child(cfg, entry, PROTOCOL_KEY, &protocol_value, true))
+    struct string_view protocol_name;
+
+    if (!cfg_get_string(cfg, entry, PROTOCOL_KEY, &protocol_name))
         return LOAD_PROTOCOL_ULTRA;
-
-    auto value = extract_string({ key_for_protocol, protocol_field.value() });
-
-    // TODO: consider making this non-case-sensitive
-    if (value != "ultra")
-        unrecoverable_error("unsupported load protocol: {}", value);
+    if (!sv_equals_caseless(protocol_name, SV("ultra")))
+        oops("unsupported load protocol: %pSV", &protocol_name);
 
     return LOAD_PROTOCOL_ULTRA;
 }
