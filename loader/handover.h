581e62677a190d0f327916bc3fd9b58b3372f5f9
8infy <99danilt@gmail.com>
2022-01-15 16:53:08 +0300

    Mark kernel_handover functions NORETURN

--- loader/handover.h
+++ loader/handover.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "common/types.h"
+#include "common/attributes.h"
 
+NORETURN
 void kernel_handover32(u32 entrypoint, u32 esp, u32 arg0, u32 arg1);
+
+NORETURN
 void kernel_handover64(u64 entrypoint, u64 rsp, u64 cr3, u64 arg0, u64 arg1);
