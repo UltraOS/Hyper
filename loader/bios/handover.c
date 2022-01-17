eb45fb2871fb1c0f835a2dd8d575fa7ac9fc3df4
8infy <99danilt@gmail.com>
2022-01-17 23:51:59 +0300

    Correct parameter order for do_kernel_handover

--- loader/bios/handover.c
+++ loader/bios/handover.c
@@ -2,7 +2,7 @@
 #include "bios_call.h"
 
 NORETURN
-void do_kernel_handover32(u32 esp, u32 entrypoint);
+void do_kernel_handover32(u32 entrypoint, u32 esp);
 
 NORETURN
 void do_kernel_handover64(u64 entrypoint, u64 rsp, u64 cr3, u64 arg0, u64 arg1);
@@ -21,7 +21,7 @@ void kernel_handover32(u32 entrypoint, u32 esp, u32 arg0, u32 arg1)
     PUSH_DWORD(esp, arg1);
     PUSH_DWORD(esp, arg0);
 
-    do_kernel_handover32(esp, entrypoint);
+    do_kernel_handover32(entrypoint, esp);
 }
 
 void kernel_handover64(u64 entrypoint, u64 rsp, u64 cr3, u64 arg0, u64 arg1)
