8a44cfb77637d5abead5e3dee0ddc7567ef55d22
8infy <99danilt@gmail.com>
2022-01-26 23:29:33 +0300

    Added new constants & simplified ROUND_UP macros

--- loader/common/constants.h
+++ loader/common/constants.h
@@ -5,11 +5,12 @@
 #define GB ((unsigned long)1024 * MB)
 
 #define PAGE_SIZE 4096
-#define PAGE_ROUND_UP(size) \
-    ((size) & (PAGE_SIZE - 1)) ? (((size) + PAGE_SIZE) & ~(PAGE_SIZE - 1)) : (size)
+#define PAGE_ROUND_UP(size) (((size) + (PAGE_SIZE - 1)) & ~(PAGE_SIZE - 1))
 #define PAGE_ROUND_DOWN(size) ((size) & ~(PAGE_SIZE - 1))
 
 #define HUGE_PAGE_SIZE (2 * MB)
-#define HUGE_PAGE_ROUND_UP(size) \
-    ((size) & (HUGE_PAGE_SIZE - 1)) ? (((size) + HUGE_PAGE_SIZE) & ~(HUGE_PAGE_SIZE - 1)) : (size)
+#define HUGE_PAGE_ROUND_UP(size) (((size) + (HUGE_PAGE_SIZE - 1)) & ~(HUGE_PAGE_SIZE - 1))
 #define HUGE_PAGE_ROUND_DOWN(size) ((size) & ~(HUGE_PAGE_SIZE - 1))
+
+#define DIRECT_MAP_BASE  0xFFFF800000000000
+#define HIGHER_HALF_BASE 0xFFFFFFFF80000000
