e4e083ce296ee83ba1f6f496de843b25b4583aa8
8infy <99danilt@gmail.com>
2022-02-02 23:50:16 +0300

    Add EFI_ERROR/EFI_ENCODE_ERROR, keep error statuses encoded

--- loader/uefi/structures.h
+++ loader/uefi/structures.h
@@ -52,40 +52,44 @@ typedef struct {
 #define EFI_WARN_FILE_SYSTEM      6
 #define EFI_WARN_RESET_REQUIRED   7
 
+#define ERROR_BIT ((UINTN)1 << ((sizeof(VOID*) * 8) - 1))
+#define EFI_ENCODE_ERROR(code) (ERROR_BIT | (code))
+#define EFI_ERROR(sts) ((INTN)sts < 0)
+
 // highest bit set
-#define EFI_LOAD_ERROR            1
-#define EFI_INVALID_PARAMETER     2
-#define EFI_UNSUPPORTED           3
-#define EFI_BAD_BUFFER_SIZE       4
-#define EFI_BUFFER_TOO_SMALL      5
-#define EFI_NOT_READY             6
-#define EFI_DEVICE_ERROR          7
-#define EFI_WRITE_PROTECTED       8
-#define EFI_OUT_OF_RESOURCES      9
-#define EFI_VOLUME_CORRUPTED     10
-#define EFI_VOLUME_FULL          11
-#define EFI_NO_MEDIA             12
-#define EFI_MEDIA_CHANGED        13
-#define EFI_NOT_FOUND            14
-#define EFI_ACCESS_DENIED        15
-#define EFI_NO_RESPONSE          16
-#define EFI_NO_MAPPING           17
-#define EFI_TIMEOUT              18
-#define EFI_NOT_STARTED          19
-#define EFI_ALREADY_STARTED      20
-#define EFI_ABORTED              21
-#define EFI_ICMP_ERROR           22
-#define EFI_TFTP_ERROR           23
-#define EFI_PROTOCOL_ERROR       24
-#define EFI_INCOMPATIBLE_VERSION 25
-#define EFI_SECURITY_VIOLATION   26
-#define EFI_CRC_ERROR            27
-#define EFI_END_OF_MEDIA         28
-#define EFI_END_OF_FILE          31
-#define EFI_INVALID_LANGUAGE     32
-#define EFI_COMPROMISED_DATA     33
-#define EFI_IP_ADDRESS_CONFLICT  34
-#define EFI_HTTP_ERROR           35
+#define EFI_LOAD_ERROR           EFI_ENCODE_ERROR(1)
+#define EFI_INVALID_PARAMETER    EFI_ENCODE_ERROR(2)
+#define EFI_UNSUPPORTED          EFI_ENCODE_ERROR(3)
+#define EFI_BAD_BUFFER_SIZE      EFI_ENCODE_ERROR(4)
+#define EFI_BUFFER_TOO_SMALL     EFI_ENCODE_ERROR(5)
+#define EFI_NOT_READY            EFI_ENCODE_ERROR(6)
+#define EFI_DEVICE_ERROR         EFI_ENCODE_ERROR(7)
+#define EFI_WRITE_PROTECTED      EFI_ENCODE_ERROR(8)
+#define EFI_OUT_OF_RESOURCES     EFI_ENCODE_ERROR(9)
+#define EFI_VOLUME_CORRUPTED     EFI_ENCODE_ERROR(10)
+#define EFI_VOLUME_FULL          EFI_ENCODE_ERROR(11)
+#define EFI_NO_MEDIA             EFI_ENCODE_ERROR(12)
+#define EFI_MEDIA_CHANGED        EFI_ENCODE_ERROR(13)
+#define EFI_NOT_FOUND            EFI_ENCODE_ERROR(14)
+#define EFI_ACCESS_DENIED        EFI_ENCODE_ERROR(15)
+#define EFI_NO_RESPONSE          EFI_ENCODE_ERROR(16)
+#define EFI_NO_MAPPING           EFI_ENCODE_ERROR(17)
+#define EFI_TIMEOUT              EFI_ENCODE_ERROR(18)
+#define EFI_NOT_STARTED          EFI_ENCODE_ERROR(19)
+#define EFI_ALREADY_STARTED      EFI_ENCODE_ERROR(20)
+#define EFI_ABORTED              EFI_ENCODE_ERROR(21)
+#define EFI_ICMP_ERROR           EFI_ENCODE_ERROR(22)
+#define EFI_TFTP_ERROR           EFI_ENCODE_ERROR(23)
+#define EFI_PROTOCOL_ERROR       EFI_ENCODE_ERROR(24)
+#define EFI_INCOMPATIBLE_VERSION EFI_ENCODE_ERROR(25)
+#define EFI_SECURITY_VIOLATION   EFI_ENCODE_ERROR(26)
+#define EFI_CRC_ERROR            EFI_ENCODE_ERROR(27)
+#define EFI_END_OF_MEDIA         EFI_ENCODE_ERROR(28)
+#define EFI_END_OF_FILE          EFI_ENCODE_ERROR(31)
+#define EFI_INVALID_LANGUAGE     EFI_ENCODE_ERROR(32)
+#define EFI_COMPROMISED_DATA     EFI_ENCODE_ERROR(33)
+#define EFI_IP_ADDRESS_CONFLICT  EFI_ENCODE_ERROR(34)
+#define EFI_HTTP_ERROR           EFI_ENCODE_ERROR(35)
 
 #define EFI_SYSTEM_TABLE_SIGNATURE 0x5453595320494249
 #define EFI_2_90_SYSTEM_TABLE_REVISION ((2 << 16) | 90)
