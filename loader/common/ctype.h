e127565157e02c11451a7d7ef0f595efed2d5703
EfeYks534 <yefeyks@gmail.com>
2022-02-06 13:18:25 +0300

    Added isalnum

--- loader/common/ctype.h
+++ loader/common/ctype.h
@@ -15,6 +15,13 @@ static inline bool islower(char c)
     return c >= 'a' && c <= 'z';
 }
 
+static inline bool isalnum(char c)
+{
+    return (c >= 'A' && c <= 'Z') ||
+           (c >= 'a' && c <= 'z') ||
+           (c >= '0' && c <= '9');
+}
+
 static inline char tolower(char c)
 {
     if (isupper(c))
