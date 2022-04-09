5c5067516a12d3eb71ac3c8a6494bfc4c1f7a004
8infy <99danilt@gmail.com>
2022-04-09 12:51:37 +0300

    installer: add support for iso MBR, failsafe against GPT + more DRY

--- installer/hyper_install.c
+++ installer/hyper_install.c
@@ -4,12 +4,16 @@
 #include <string.h>
 #include <stdint.h>
 #include <errno.h>
+#include <stdarg.h>
 
 extern char mbr_data[];
-extern uint32_t mbr_size;
+extern unsigned long mbr_size;
+
+extern char iso_mbr_data[];
+extern unsigned long iso_mbr_size;
 
 extern char stage2_data[];
-extern uint32_t stage2_size;
+extern unsigned long stage2_size;
 
 #define MBR_BLOCK_SIZE               512
 #define MBR_MAGIC                    0xAA55
@@ -26,147 +30,161 @@ struct mbr_partition_entry {
 };
 #define MBR_PARTITION_COUNT 4
 
-bool read_at(FILE *f, size_t offset, void *buf, size_t bytes)
+_Noreturn
+#ifdef __GNUC__
+__attribute__((format(printf, 1, 2)))
+#endif
+void panic(const char *err, ...)
 {
-    if (fseek(f, offset, SEEK_SET) != 0) {
-        fprintf(stderr, "Failed to seek file to %zu:%s\n", offset, strerror(errno));
-        return false;
-    } 
+    va_list vl;
 
-    if (fread(buf, 1, bytes, f) != bytes) {
-        fprintf(stderr, "Failed to read %zu bytes at %zu: %s\n", bytes, offset, strerror(errno));
-        return false;
-    }
+    va_start(vl, err);
+    vfprintf(stderr, err, vl);
+    va_end(vl);
 
-    return true;
+    exit(1);
 }
 
-bool write_at(FILE *f, size_t offset, void *buf, size_t bytes)
+FILE *safe_open(const char *path, const char *mode)
 {
-    if (fseek(f, offset, SEEK_SET) != 0) {
-        fprintf(stderr, "Failed to seek file to %zu:%s\n", offset, strerror(errno));
-        return false;
-    } 
+    FILE *out = fopen(path, mode);
+    if (out)
+        return out;
 
-    if (fwrite(buf, 1, bytes, f) != bytes) {
-        fprintf(stderr, "Failed to write %zu bytes at %zu: %s\n", bytes, offset, strerror(errno));
-        return false;
-    }
-
-    return true;
+    panic("Failed to open file %s: %s\n", path, strerror(errno));
 }
 
-bool read_mbr_partition_list(const char *path, struct mbr_partition_entry *buffer)
+void safe_read_at(FILE *f, size_t offset, void *buf, size_t bytes)
 {
-    FILE *f = fopen(path, "rb");
+    if (fseek(f, offset, SEEK_SET) != 0)
+        panic("Failed to seek file to %zu:%s\n", offset, strerror(errno));
+
+    if (fread(buf, 1, bytes, f) != bytes)
+        panic("Failed to read %zu bytes at %zu: %s\n", bytes, offset, strerror(errno));
+}
+
+void safe_write_at(FILE *f, size_t offset, void *buf, size_t bytes)
+{
+    if (fseek(f, offset, SEEK_SET) != 0)
+        panic("Failed to seek file to %zu:%s\n", offset, strerror(errno));
+
+    if (fwrite(buf, 1, bytes, f) != bytes)
+        panic("Failed to write %zu bytes at %zu: %s\n", bytes, offset, strerror(errno));
+}
+
+void read_mbr_partition_list(FILE *img, struct mbr_partition_entry *buffer)
+{
     uint16_t mbr_magic;
-    size_t bytes_read;
-    bool ret = false;
-    
-    if (!f) {
-        fprintf(stderr, "Failed to open %s: %s\n", path, strerror(errno));
-        goto out;
-    }
 
-    if (!read_at(f, OFFSET_TO_MBR_MAGIC, &mbr_magic, sizeof(mbr_magic)))
-        goto out;
+    safe_read_at(img, OFFSET_TO_MBR_MAGIC, &mbr_magic, sizeof(mbr_magic));
 
-    if (mbr_magic != MBR_MAGIC) {
-        fprintf(stderr, "Invalid MBR magic, expected 0x%04hX got 0x%04hX\n", MBR_MAGIC, mbr_magic);
-        goto out;
-    }
+    if (mbr_magic != MBR_MAGIC)
+        panic("Invalid MBR magic, expected 0x%04hX got 0x%04hX\n", MBR_MAGIC, mbr_magic);
 
-    if (!read_at(f, OFFSET_TO_MBR_PARTITION_LIST, buffer, MBR_PARTITION_COUNT * sizeof(struct mbr_partition_entry)))
-        goto out;
-
-    ret = true;
-
-out:
-    if (f)
-        fclose(f);
-
-    return ret;
+    safe_read_at(img, OFFSET_TO_MBR_PARTITION_LIST, buffer,
+                 MBR_PARTITION_COUNT * sizeof(struct mbr_partition_entry));
 }
 
-bool check_stage2_fits(struct mbr_partition_entry *partitons)
+void ensure_stage2_fits(struct mbr_partition_entry *partitions)
 {
-    size_t i = 0;
+    size_t i;
     uint32_t lowest_block = UINT32_MAX;
     size_t gap_size;
 
     for (i = 0; i < MBR_PARTITION_COUNT; ++i) {
-        uint32_t first_block = partitons[i].first_block;
+        uint32_t first_block = partitions[i].first_block;
 
-        if (partitons[i].first_block < lowest_block)
-            lowest_block = partitons[i].first_block;
+        if (first_block && (first_block < lowest_block))
+            lowest_block = first_block;
     }
 
     gap_size = (lowest_block - 1) * MBR_BLOCK_SIZE;
 
     if (gap_size < stage2_size) {
-        fprintf(stderr, "Not enough space between MBR and the first partition to fit stage2!\n"
-                        "Need at least %u, have %zu\n", stage2_size, gap_size);
-        return false;
+        panic("Not enough space between MBR and the first partition to fit stage2!\n"
+              "Need at least %lu, have %zu\n", stage2_size, gap_size);
     }
-
-    return true;
 }
 
-bool write_mbr(FILE *f, struct mbr_partition_entry *partitions)
+void write_mbr(FILE *f, struct mbr_partition_entry *partitions, bool is_iso)
 {
-    if (!write_at(f, 0, mbr_data, mbr_size))
-        return false;
+    void *data = is_iso ? iso_mbr_data : mbr_data;
+    size_t size = is_iso ? iso_mbr_size : mbr_size;
 
-    return write_at(f, OFFSET_TO_MBR_PARTITION_LIST, partitions,
-                    sizeof(struct mbr_partition_entry) * MBR_PARTITION_COUNT);
+    safe_write_at(f, 0, data, size);
+    safe_write_at(f, OFFSET_TO_MBR_PARTITION_LIST, partitions,
+                  sizeof(struct mbr_partition_entry) * MBR_PARTITION_COUNT);
 }
 
-bool write_stage2(FILE *f)
+void write_stage2(FILE *f)
 {
-    return write_at(f, MBR_BLOCK_SIZE, stage2_data, stage2_size);
+    safe_write_at(f, MBR_BLOCK_SIZE, stage2_data, stage2_size);
 }
 
-bool write_hyper(const char *path, struct mbr_partition_entry *mbr_partitions)
+void write_hyper(FILE *img, struct mbr_partition_entry *mbr_partitions, bool is_iso)
 {
-    FILE *f = fopen(path, "r+b");
-    bool ret = false;
+    write_mbr(img, mbr_partitions, is_iso);
 
-    if (!f) {
-        fprintf(stderr, "Failed to open %s: %s\n", path, strerror(errno));
-        goto out;
-    }
+    if (!is_iso)
+        write_stage2(img);
+}
 
-    if (!write_mbr(f, mbr_partitions))
-        goto out;
-    
-    if (!write_stage2(f))
-        goto out;
+#define ISO9660_LOGICAL_BLOCK_SIZE 2048
+#define ISO9660_SYSTEM_AREA_BLOCKS 16
+#define ISO9660_PVD_OFF (ISO9660_LOGICAL_BLOCK_SIZE * ISO9660_SYSTEM_AREA_BLOCKS + 1)
+#define ISO9660_IDENTIFIER "CD001"
 
-    ret = true;
+bool is_iso_disk(FILE *img)
+{
+    char data[sizeof(ISO9660_IDENTIFIER) - 1];
+    safe_read_at(img, ISO9660_PVD_OFF, data, sizeof(ISO9660_IDENTIFIER) - 1);
 
-out:
-    if (f)
-       fclose(f);
-    return ret;
+    return memcmp(data, ISO9660_IDENTIFIER, sizeof(ISO9660_IDENTIFIER) - 1) == 0;
+}
+
+// "EFI PART"
+#define GPT_SIGNATURE 0x5452415020494645
+
+void ensure_no_gpt(FILE *img)
+{
+    uint64_t signature;
+
+    safe_read_at(img, 512, &signature, sizeof(signature));
+    if (signature == GPT_SIGNATURE)
+        goto out_gpt;
+
+    safe_read_at(img, 4096, &signature, sizeof(signature));
+    if (signature == GPT_SIGNATURE)
+        goto out_gpt;
+
+    return;
+
+out_gpt:
+    panic("Installing to GPT images is currently not supported\n");
 }
 
 int main(int argc, char **argv)
 {
     struct mbr_partition_entry mbr_partitions[MBR_PARTITION_COUNT];
+    bool is_iso;
+    FILE *img;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <path-where-to-install>\n", argv[0]);
         return 1;
     }
+    img = safe_open(argv[1], "r+b");
 
-    if (!read_mbr_partition_list(argv[1], mbr_partitions))
-        return 1;
+    // Currently unsupported
+    ensure_no_gpt(img);
 
-    if (!check_stage2_fits(mbr_partitions))
-        return 1;
+    read_mbr_partition_list(img, mbr_partitions);
+    is_iso = is_iso_disk(img);
 
-    if (!write_hyper(argv[1], mbr_partitions))
-        return 1;
+    if (!is_iso)
+        ensure_stage2_fits(mbr_partitions);
 
+    write_hyper(img, mbr_partitions, is_iso);
+    fclose(img);
     return 0;
 }
