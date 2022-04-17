b6d849c8a48f691bc4ad929bf445705dbedae9ef
8infy <99danilt@gmail.com>
2022-04-17 13:02:13 +0300

    common/bug: add DIE/DIE_ON

--- loader/common/bug.h
+++ loader/common/bug.h
@@ -7,9 +7,16 @@
 #define BUILD_BUG_ON(expr) BUILD_BUG_ON_WITH_MSG(expr, "BUILD BUG: " #expr " evaluated to true")
 
 #define BUG() panic("BUG! At %s() in file %s:%d\n", __func__, __FILE__, __LINE__)
+#define DIE() panic("Unrecoverable error! At %s() in file %s:%d\n", __func__, __FILE__, __LINE__)
 
 #define BUG_ON(expr)        \
     do {                    \
         if (unlikely(expr)) \
             BUG();          \
+    } while (0)
+
+#define DIE_ON(expr)        \
+    do {                    \
+        if (unlikely(expr)) \
+            DIE();          \
     } while (0)
