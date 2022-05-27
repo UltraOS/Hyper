35af1c749654e0f996be84a2af0b2d39b2ad9fce
8infy <99danilt@gmail.com>
2022-05-27 12:31:49 +0300

    common/range: remove range.c and all now unused helpers

--- loader/common/range.h
+++ loader/common/range.h
@@ -7,28 +7,9 @@ struct range {
     u64 end;
 };
 
-void range_align_start(struct range *range, size_t alignment);
-void range_align_length(struct range *range, size_t alignment);
-void range_align(struct range *range, size_t alignment);
-
 static inline void range_advance_begin(struct range *range, u64 by)
 {
     range->begin += by;
-}
-
-static inline bool range_contains(const struct range *lhs, const struct range *rhs)
-{
-    return rhs->begin >= lhs->begin && rhs->end <= lhs->end;
-}
-
-static inline bool range_overlaps(const struct range *lhs, const struct range *rhs)
-{
-    return rhs->begin >= lhs->begin && rhs->begin < lhs->end;
-}
-
-static inline bool range_is_empty(const struct range *range)
-{
-    return (range->end - range->begin) == 0;
 }
 
 static inline void range_set_length(struct range *range, u64 length)
