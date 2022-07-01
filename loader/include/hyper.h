2dc8d03745d3dd9cd9f096b5c5cda53a49a67808
8infy <99danilt@gmail.com>
2022-07-01 18:01:00 +0300

    hyper: release 0.3.1

--- loader/include/hyper.h
+++ loader/include/hyper.h
@@ -5,7 +5,7 @@
 
 #define HYPER_MAJOR 0
 #define HYPER_MINOR 3
-#define HYPER_PATCH 0
+#define HYPER_PATCH 1
 
 #define MAKE_BRAND_STRING(mj, mi, pa) SV("HyperLoader v" TO_STR(mj) "." TO_STR(mi) "." TO_STR(pa))
 #define HYPER_BRAND_STRING MAKE_BRAND_STRING(HYPER_MAJOR, HYPER_MINOR, HYPER_PATCH)
