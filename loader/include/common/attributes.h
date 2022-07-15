a831203b6ee57f9940875696699dedd95dba8dde
8infy <99danilt@gmail.com>
2022-07-15 15:19:14 +0300

    common/attributes: replace _Noreturn with a GCC attribute
    The GCC attribute is a lot more useful, as it can be applied on function
    pointers as well, whereas _Noreturn can only be used for declarations.

--- loader/include/common/attributes.h
+++ loader/include/common/attributes.h
@@ -2,7 +2,7 @@
 
 #define PACKED __attribute__((packed))
 
-#define NORETURN _Noreturn
+#define NORETURN __attribute__((__noreturn__))
 
 #ifdef __clang__
 #define PRINTF_DECL(fmt_idx, args_idx) __attribute__((format(printf, fmt_idx, args_idx)))
