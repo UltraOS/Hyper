c65d2061eeb426361076c820b4e66326b8ebad3e
8infy <99danilt@gmail.com>
2022-09-24 15:52:38 +0300

    tests/kernel: make is_in_hypervisor detection on-demand
    This way we don't have to rely on the initialization code to run before
    we are able to use 0xE9 logging.

--- tests/kernel/test_ctl.c
+++ tests/kernel/test_ctl.c
@@ -17,18 +17,26 @@
 #define TEST_FAIL_MARKER2 0xBE
 #define TEST_FAIL_MARKER3 0xEF
 
-static bool is_in_hypervisor = false;
+static int is_in_hypervisor_state = -1;
 static bool should_shutdown = true;
 
 #define HYPERVISOR_BIT (1 << 31)
 
+static bool is_in_hypervisor(void)
+{
+    if (is_in_hypervisor_state == -1) {
+        struct cpuid_res res;
+
+        cpuid(1, &res);
+        is_in_hypervisor_state = res.c & HYPERVISOR_BIT;
+    }
+
+    return is_in_hypervisor_state;
+}
+
 void test_ctl_init(struct ultra_boot_context *bctx)
 {
     struct ultra_command_line_attribute *cmdline;
-    struct cpuid_res res;
-
-    cpuid(1, &res);
-    is_in_hypervisor = res.c & HYPERVISOR_BIT;
 
     cmdline = (struct ultra_command_line_attribute*)find_attr(bctx, ULTRA_ATTRIBUTE_COMMAND_LINE);
     if (cmdline)
@@ -48,7 +56,7 @@ static inline void write_0xe9(const char *str, size_t len)
 
 void test_write_string(const char *str, size_t count)
 {
-    if (is_in_hypervisor)
+    if (is_in_hypervisor())
         write_0xe9(str, count);
 
     fb_tty_write(str, count);
@@ -68,7 +76,7 @@ void test_vfail(const char *reason, va_list vlist)
 {
     vprint(reason, vlist);
 
-    if (is_in_hypervisor && should_shutdown) {
+    if (is_in_hypervisor() && should_shutdown) {
         put_0xe9(TEST_FAIL_MARKER0);
         put_0xe9(TEST_FAIL_MARKER1);
         put_0xe9(TEST_FAIL_MARKER2);
@@ -108,7 +116,7 @@ void test_pass()
 {
     print("TEST PASS!\n");
 
-    if (is_in_hypervisor && should_shutdown) {
+    if (is_in_hypervisor() && should_shutdown) {
         put_0xe9(TEST_PASS_MARKER0);
         put_0xe9(TEST_PASS_MARKER1);
         put_0xe9(TEST_PASS_MARKER2);
