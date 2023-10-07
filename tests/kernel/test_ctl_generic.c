41aceb10a818ac6114f888b07312079962f7d69f
8infy <99danilt@gmail.com>
2023-10-07 16:36:10 +0300

    tests/test_ctl: require even less arch-specific code
    With this commit, every arch is only required to implement 2 helpers:
    - arch_put_byte (write a byte to stdout)
    - arch_shutdown_or_hang
    - (optionally) arch_write_string, if a fast path exists

--- tests/kernel/test_ctl_generic.c
+++ tests/kernel/test_ctl_generic.c
@@ -1,6 +1,41 @@
 #include <stdarg.h>
+#include "common/attributes.h"
+#include "fb_tty.h"
 #include "test_ctl.h"
 #include "test_ctl_impl.h"
+
+#define TEST_PASS_MARKER0 0xCA
+#define TEST_PASS_MARKER1 0xFE
+#define TEST_PASS_MARKER2 0xBA
+#define TEST_PASS_MARKER3 0xBE
+
+#define TEST_FAIL_MARKER0 0xDE
+#define TEST_FAIL_MARKER1 0xAD
+#define TEST_FAIL_MARKER2 0xBE
+#define TEST_FAIL_MARKER3 0xEF
+
+void test_pass()
+{
+    print("TEST PASS!\n");
+
+    arch_put_byte(TEST_PASS_MARKER0);
+    arch_put_byte(TEST_PASS_MARKER1);
+    arch_put_byte(TEST_PASS_MARKER2);
+    arch_put_byte(TEST_PASS_MARKER3);
+    arch_hang_or_shutdown();
+}
+
+void test_vfail(const char *reason, va_list vlist)
+{
+    print("TEST FAIL!\n");
+    vprint(reason, vlist);
+
+    arch_put_byte(TEST_FAIL_MARKER0);
+    arch_put_byte(TEST_FAIL_MARKER1);
+    arch_put_byte(TEST_FAIL_MARKER2);
+    arch_put_byte(TEST_FAIL_MARKER3);
+    arch_hang_or_shutdown();
+}
 
 void test_fail(const char *reason, ...)
 {
@@ -24,4 +59,17 @@ void oops(const char *reason, ...)
     va_start(vlist, reason);
     test_vfail(reason, vlist);
     va_end(vlist);
+}
+
+WEAK
+void arch_write_string(const char *str, size_t count)
+{
+    while (count--)
+        arch_put_byte(*str++);
+}
+
+void test_write_string(const char *str, size_t count)
+{
+    arch_write_string(str, count);
+    fb_tty_write(str, count);
 }
--- tests/kernel/test_ctl_impl.h
+++ tests/kernel/test_ctl_impl.h
@@ -1,14 +1,12 @@
 #pragma once
-#include <stdarg.h>
 
-#define TEST_PASS_MARKER0 0xCA
-#define TEST_PASS_MARKER1 0xFE
-#define TEST_PASS_MARKER2 0xBA
-#define TEST_PASS_MARKER3 0xBE
+#include "common/attributes.h"
 
-#define TEST_FAIL_MARKER0 0xDE
-#define TEST_FAIL_MARKER1 0xAD
-#define TEST_FAIL_MARKER2 0xBE
-#define TEST_FAIL_MARKER3 0xEF
+void arch_put_byte(char c);
 
-void test_vfail(const char *reason, va_list vlist);
+// Optional, arch_put_byte is called per char if not implemented
+void arch_write_string(const char *str, size_t count);
+
+// *MUST* shutdown to make tests pass, allowed to hang if on real HW
+NORETURN
+void arch_hang_or_shutdown(void);
