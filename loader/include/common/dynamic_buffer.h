d70f0a391054f54210423b74da61cb7bcabbae02
Daniil Tatianin <99danilt@gmail.com>
2026-07-13 00:20:16 +0300

    common/dynamic_buffer: make get_slot accept a constant buffer
    No reason to have the argument be non-constant as the buffer state is
    not mutated in this function.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- loader/include/common/dynamic_buffer.h
+++ loader/include/common/dynamic_buffer.h
@@ -29,7 +29,8 @@ static inline bool dynamic_buffer_init(struct dynamic_buffer *db, size_t elem_si
     return lazy ? true : dynamic_buffer_grow(db);
 }
 
-static inline void *dynamic_buffer_get_slot(struct dynamic_buffer *db, size_t i)
+static inline void *dynamic_buffer_get_slot(const struct dynamic_buffer *db,
+                                            size_t i)
 {
     BUG_ON(i >= db->size);
     return db->buf + (i * db->elem_size);
