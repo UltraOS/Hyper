37791d0456a8cbfed66b44835f0e850297b54306
Daniil Tatianin <99danilt@gmail.com>
2026-07-17 17:57:35 +0300

    uefi/helpers: add uefi_get_protocol_handles_nowarn
    Some callers of uefi_get_protocol_handles _do_ expect that it may fail
    and that's totally normal, e.g. getting a PXE handle under non-PXE boot.
    
    Add a helper so we can avoid generating a warning for such cases.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- loader/include/uefi/helpers.h
+++ loader/include/uefi/helpers.h
@@ -14,6 +14,13 @@ bool uefi_pool_alloc(EFI_MEMORY_TYPE type, size_t elem_size, size_t count, VOID 
  */
 bool uefi_get_protocol_handles(EFI_GUID *guid, EFI_HANDLE **array, UINTN *count);
 
+/*
+ * Same as uefi_get_protocol_handles, except no warning is generated in case
+ * no handles are found.
+ */
+bool uefi_get_protocol_handles_nowarn(EFI_GUID *guid, EFI_HANDLE **array,
+                                      UINTN *count);
+
 struct string_view uefi_status_to_string(EFI_STATUS sts);
 
 void *uefi_find_configuration(EFI_GUID *guid);
